67d0619b76a8c1b1e466dd6f31c5fbf035ba6f79
TIEN CHIH CHENG <aim840912@gmail.com>
2022-08-08 21:51:00 +0800

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float torqueAmount = 1f;
+
+    [SerializeField]
+    float boostSpeed = 30f;
+
+    [SerializeField]
+    float baseSpeed = 20f;
+    Rigidbody2D rb2d;
+
+    SurfaceEffector2D surfaceEffector2D;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+    }
+
+    void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# SnowBoarder
+
+## Learn to use
+
+Sprite Shapes
+- Sprite Shape Controller
+
+CineMachine
+- cinemachine => vistual Camera
+
+Surface Effector2D
+
+Rigidbody2D
+- AddTorque();
+
+Particle System
